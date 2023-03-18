22a3c8c2cbe9b7866275be4f957cf2f765fd5e7c
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-03-18 14:14:25 +0500

    Add js

--- script.js
+++ script.js
@@ -49,9 +49,36 @@ function updateProgress() {
   duration.textContent = `${displayTime(video.duration)}`;
 }
 
+// 
+function setProgress(e) {  
+  const newTime = e.offsetX / progressRange.offsetWidth;
+  progressBar.style.width = `${newTime * 100}%`;
+  video.currentTime = newTime * video.duration;
+}
+
 
 // Volume Controls --------------------------- //
+function changeVolume(e) {
+  let volume = e.offsetX / volumeRange.offsetWidth;
+  if (volume < 0.1) {
+    volume = 0;
+  }
+  if (volume > 0.9) {
+    volume = 1;
+  }
 
+  volumeBar.style.width = `${volume * 100}%`
+  video.volume = volume;
+
+  volumeIcon.className = '';
+  if (volume > 0.7) {
+    volumeIcon.classList.add('fas', 'fa-volume-up')
+  } else if (volume < 0.7 && volume > 0) {
+    volumeIcon.classList.add('fas', 'fa-volume-down')
+  } else if (volume === 0 ) {
+    volumeIcon.classList.add('fas', 'fa-volume-off')
+  }
+}
 
 
 // Change Playback Speed -------------------- //
@@ -66,3 +93,7 @@ playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
 video.addEventListener('timeupdate', updateProgress);
 video.addEventListener('canplay', updateProgress);
+progressRange.addEventListener('click', setProgress);
+volumeRange.addEventListener('click', changeVolume);
+
+
