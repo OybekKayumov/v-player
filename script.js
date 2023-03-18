f0686eb08af78bc89122e5bfc702a9ecf08f4d75
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-03-18 12:56:17 +0500

    Add js

--- script.js
+++ script.js
@@ -39,12 +39,14 @@ function displayTime(time) {
   const minutes = Math.floor(time / 60);
   let seconds = Math.floor(time % 60);
   seconds = seconds > 9 ? seconds : `0${seconds}`;
+  return `${minutes}:${seconds}`;
 }
 
 // update progress bar
 function updateProgress() {  
   progressBar.style.width = `${( video.currentTime / video.duration) * 100}%`;
-
+  currentTime.textContent = `${displayTime(video.currentTime)} / `;
+  duration.textContent = `${displayTime(video.duration)}`;
 }
 
 
